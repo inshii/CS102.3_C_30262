7ca6c6fb8dc4ee2ad7566858d64feb24992a7fc5
inshii <135641481+inshii@users.noreply.github.com>
2023-07-31 09:28:34 +0530

    C 2023.06.05 Lab

---
+++ C-Lab-2023.06.05/2023.06.05/Qs1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int no1,no2,div;
+    printf("Enter two number");
+    scanf("%d %d",&no1,&no2);
+
+    if((no1%no2)==0)
+        printf("is a multiple");
+    else
+       printf("not a multiple");
+
+
+
+}
